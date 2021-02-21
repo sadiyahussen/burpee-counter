5215147a1ae5eb36fa3f61889bca036ca10e2316
sadiyahussen <79389858+sadiyahussen@users.noreply.github.com>
2021-02-20 22:36:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,116 @@
+basic.showString("Ready")
+basic.showString("Set")
+basic.showString("GO!!")
+basic.showNumber(1)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(2)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(3)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(4)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(5)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(6)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(7)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(8)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(9)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.showNumber(10)
+basic.pause(200)
+basic.showString("Yay!")
+basic.showString("You did it!")
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . .
+    # # . . .
+    # # # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . . .
+    # . # . .
+    . # . # .
+    . . . # .
+    `)
+basic.showLeds(`
+    # . . # .
+    . # . . #
+    . . . # .
+    . . . . .
+    . . . . #
+    `)
